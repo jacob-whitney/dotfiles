ff9a146c2780b546012ba41e4f6217a0d0e5a910
Jacob Whitney (OCLS Desktop) <jwhitney@ocls.info>
2024-11-04 10:58:47 -0500

    Add setup.sh, setup script to add dotfiles to user

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# Check that git directory is there
+
+# Check if .bashrc, .vimrc, .taskrc, .task or any other files in dotfiles repo are in home directory
+
+# If yes to both, complete a diff on the two files
+
+# Check with user to see if they are okay with the changes between original and new from dotfiles repo
+
+# If okay, delete old files. Or just move/archive them.
+
+# Then symlink new files
+
+# For Taskwarrior, install taskwarrior
+  # This could be a separate file
+  # Each file could be separate script
